1c003a4f8d99f8fe3df62e4c8d4afd075abebd95
maxwell <hca19981208@163.com>
2019-11-27 17:25:03 +0800

    replace TAB with Space

--- ip.py
+++ ip.py
@@ -5,32 +5,32 @@
 # 回溯算法
 '''
 def restoreIpAdress(s):
-	def valid(segment):
-		return int(segment) < 256 if segment[0] != '0' else len(segment) == 1
+    def valid(segment):
+        return int(segment) < 256 if segment[0] != '0' else len(segment) == 1
 
-	def update_output(curr_pos):
-		segment = s[curr_pos + 1:]
-		if valid(segment):
-			segments.append(segment)
-			output.append('.'.join(segments))
-			segments.pop()
+    def update_output(curr_pos):
+        segment = s[curr_pos + 1:]
+        if valid(segment):
+            segments.append(segment)
+            output.append('.'.join(segments))
+            segments.pop()
 
-	def backtrack(prev_pos = -1, dots = 3):
-		for curr_pos in range(prev_pos + 1, min(n - 1, prev_pos + 4)):
-			segment = s[prev_pos + 1:curr_pos + 1]
-			if valid(segment):
-				segments.append(segment)
-				if dots == 1:
-					update_output(curr_pos)
-				else:
-					backtrack(curr_pos, dots - 1)
-				segments.pop()
+    def backtrack(prev_pos = -1, dots = 3):
+        for curr_pos in range(prev_pos + 1, min(n - 1, prev_pos + 4)):
+            segment = s[prev_pos + 1:curr_pos + 1]
+            if valid(segment):
+                segments.append(segment)
+                if dots == 1:
+                    update_output(curr_pos)
+                else:
+                    backtrack(curr_pos, dots - 1)
+                segments.pop()
 
-	n = len(s)
-	segments, output = [], []
-	backtrack()
-	return output
+    n = len(s)
+    segments, output = [], []
+    backtrack()
+    return output
 
 if __name__ == "__main__":
-	ip = "82937418"
-	print(restoreIpAdress(ip))
+    ip = "25525501"
+    print(restoreIpAdress(ip))
--- queen.py
+++ queen.py
@@ -5,38 +5,38 @@
 # 回溯算法
 '''
 def NQueen(n):
-	def valid(curr_row, curr_col):
-		for row, col in enumerate(cols):
-			if col == curr_col or row + col == curr_row + curr_col or row - col == curr_row - curr_col:
-				return False
-		return True
+    def valid(curr_row, curr_col):
+        for row, col in enumerate(cols):
+            if col == curr_col or row + col == curr_row + curr_col or row - col == curr_row - curr_col:
+                return False
+        return True
 
-	def update_output():
-		segment = []
-		for col in cols:
-			s = ''
-			for i in range(n):
-				if i == col:
-					s += 'Q'
-				else:
-					s += '.'
-			segment.append(s)
-		output.append(segment)
+    def update_output():
+        segment = []
+        for col in cols:
+            s = ''
+            for i in range(n):
+                if i == col:
+                    s += 'Q'
+                else:
+                    s += '.'
+            segment.append(s)
+        output.append(segment)
 
-	def backtrack(curr_row = 0):
-		for curr_col in range(n): 
-			if valid(curr_row, curr_col):
-				cols.append(curr_col)
-				if curr_row == n - 1:
-					update_output()
-				else:
-					backtrack(curr_row + 1)
-				cols.pop()
+    def backtrack(curr_row = 0):
+        for curr_col in range(n): 
+            if valid(curr_row, curr_col):
+                cols.append(curr_col)
+                if curr_row == n - 1:
+                    update_output()
+                else:
+                    backtrack(curr_row + 1)
+                cols.pop()
 
-	cols, output = [], []
-	backtrack()
-	return output
+    cols, output = [], []
+    backtrack()
+    return output
 
 if __name__ == "__main__":
-	n = 8
-	print(len(NQueen(n)))
+    n = 8
+    print(len(NQueen(n)))
